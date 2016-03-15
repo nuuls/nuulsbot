2fb26d9483caeaa0efc8f9c09f486f2f14bb1aae
nuuls <nilsv21@gmail.com>
2016-03-16 00:30:00 +0100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,9 @@
+HOST = "irc.chat.twitch.tv"
+PORT = 80
+WHISPERHOST = "199.9.253.119"
+WHISPERPORT = 443
+PASS = "oauth:Kappa"
+IDENT = "nuulsbot"
+CHANNEL = input("channel: ")
+ADMINS = ["nuuls", "nickfoon"]
+BANPHRASES = [".", "superlongmessage", "longmessage"]
