7b7e3dae29ffba6e7d6d108a964c8b2af93bce3f
nuuls <nilsv21@gmail.com>
2016-03-16 00:27:56 +0100

    Keepo

--- settings.py
+++
@@ -1,9 +0,0 @@
-HOST = "irc.chat.twitch.tv"
-PORT = 80
-WHISPERHOST = "199.9.253.119"
-WHISPERPORT = 443
-PASS = "oauth:xDDDDDDDDDDD"
-IDENT = "nuulsbot"
-CHANNEL = input("channel: ")
-ADMINS = ["nuuls", "nickfoon"]
-BANPHRASES = [".", "superlongmessage", "longmessage"]
